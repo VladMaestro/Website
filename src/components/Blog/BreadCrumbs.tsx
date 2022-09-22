a6e03665aa7137a72c04e88483048fdc3752fb71
Артём Одийко <odiykoartem@gmail.com>
2022-09-22 19:42:14 +0300

    Added component for bread crumbs

---
+++ src/components/Blog/BreadCrumbs.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export const BreadCrumbs: React.FC = () => {
+	const router = useRouter();
+	const breadCrumbs = router.asPath.split("/");
+
+	const arr: React.ReactNode[] = [];
+	let breadCrumbsString = "/";
+
+	breadCrumbs.map((item, index) => {
+		if (!item.length) {
+			arr.push(
+				<p key={index} className="breadCrumbs__item">
+					<Link href={breadCrumbsString}>
+						<a className="breadCrumbs__link">Home</a>
+					</Link>
+					<span className="breadCrumbs__slash">/</span>
+				</p>
+			);
+		} else {
+			breadCrumbsString = breadCrumbsString + `${item}/`;
+			arr.push(
+				<p key={index} className="breadCrumbs__item">
+					<Link href={`${breadCrumbsString}`}>
+						<a className="breadCrumbs__link">{item.charAt(0).toUpperCase() + item.slice(1)}</a>
+					</Link>
+					<span className="breadCrumbs__slash">/</span>
+				</p>
+			);
+		}
+	});
+
+	return <div className="breadCrumbs">{arr}</div>;
+};
