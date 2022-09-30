11320eff77a22b1e9d9cb2383ca2664c0149bc21
Артём Одийко <odiykoartem@gmail.com>
2022-09-30 21:48:50 +0300

    Puted data from contentful

--- src/components/Blog/BigArticle.tsx
+++ src/components/Blog/BigArticle.tsx
@@ -1,32 +1,38 @@
-import React from "react";
+import { FC } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-export const BigArticle: React.FC = () => {
+import { bigArticleType } from "../../@types/articles";
+import { formatDate } from "../../utils/formatDate";
+
+type BigArticleProps = {
+	data: bigArticleType;
+};
+
+export const BigArticle: FC<BigArticleProps> = ({ data }) => {
 	return (
 		<article className="bigArticle">
 			<div className="bigArticle__pictire">
 				<Image
-					src="/img/lesson/lesson.jpg"
-					alt="Mountains"
+					src={data.previewImg.url}
+					alt={data.previewImg.title}
+					placeholder="blur"
+					blurDataURL="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSI+PGRlZnM+PGxpbmVhckdyYWRpZW50IGlkPSJnMSIgZ3JhZGllbnRVbml0cz0idXNlclNwYWNlT25Vc2UiIHgxPSIwJSIgeTE9IjEwMCUiIHgyPSIxMDAlIiB5Mj0iMCUiPjxzdG9wIG9mZnNldD0iMCUiIHN0b3AtY29sb3I9IiNkNWQ1ZDUiLz48c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNlMmUyZTIiLz48L2xpbmVhckdyYWRpZW50PjwvZGVmcz48cmVjdCB4PSIwIiB5PSIwIiB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJ1cmwoI2cxKSIvPjwvc3ZnPg=="
+					style={{ borderRadius: 10 }}
 					layout="fill"
 					objectFit="cover"
 					className="bigArticle__img"
 				/>
 			</div>
 			<div className="bigArticle__info">
-				<span className="bigArticle__tag">Grammer</span>
+				<span className="bigArticle__tag">{data.tag.name}</span>
 				<h1 className="bigArticle__title" id="bigArticleTitle">
-					<Link href="/blog/article/test">
-						<a className="bigArticle__link">
-							Hernandez leads Blue Jays to wild win over Rangers in Toronto`s 1st home opener since 2019
-						</a>
+					<Link href={`/blog/article/${data.slug}`}>
+						<a className="bigArticle__link">{data.title}</a>
 					</Link>
 				</h1>
-				<p className="bigArticle__description">
-					Rogers Center sees 1st sellout since March 29, 2018 for Totonto`s 10-8 comeback win
-				</p>
-				<time className="time">9 April 2022</time>
+				<p className="bigArticle__description">{data.smallDescription}</p>
+				<time className="time">{formatDate(data.sys.publishedAt)}</time>
 			</div>
 		</article>
 	);
