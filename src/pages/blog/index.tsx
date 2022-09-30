faad27e3ddae7130359904b24f1f89b72d0b02bb
Артём Одийко <odiykoartem@gmail.com>
2022-09-30 21:41:58 +0300

    Fetching data for main page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,16 +1,43 @@
-import React, { ReactElement } from "react";
+import { ReactElement } from "react";
 
 import type { NextPageWithLayout } from "../_app";
 import { BlogLayout } from "../../Layouts/BlogLayout";
-import { AllSections, LastArticles, Tags, Toefl } from "../../components";
+import { AllSections, LastArticles, Subscribe, Tags, Toefl } from "../../components";
 
-const Blog: NextPageWithLayout = () => {
+import { getFirst5PostsSlugs, getMainPageData } from "../../contentful";
+
+import { MainPageQuery } from "../../@types/contentfulSchema";
+
+export const getStaticProps = async () => {
+	const slugs = await getFirst5PostsSlugs();
+	const fiveSlugs = slugs.postCollection.items.map((item) => item.slug);
+
+	const mainPageData = await getMainPageData(fiveSlugs);
+
+	return {
+		props: {
+			mainPageData,
+		},
+	};
+};
+
+type BlogProps = {
+	mainPageData: MainPageQuery;
+};
+
+const Blog: NextPageWithLayout<BlogProps> = ({ mainPageData }) => {
 	return (
 		<>
-			<Tags />
-			<LastArticles />
-			<Toefl />
-			<AllSections />
+			<Tags data={mainPageData.tagCollection.items} />
+			<LastArticles posts={mainPageData.posts.items} recommendedPosts={mainPageData.recommended.items} />
+			<Toefl data={mainPageData.toefl.items} />
+			<AllSections
+				grammar={mainPageData.grammar.items}
+				usCulture={mainPageData.usCulture.items}
+				det={mainPageData.det.items}
+				speakingTips={mainPageData.speakingTips.items}
+			/>
+			<Subscribe />
 		</>
 	);
 };
