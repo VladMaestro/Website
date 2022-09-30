36fc140aac946fc5232190025cf0db977f160236
Артём Одийко <odiykoartem@gmail.com>
2022-09-30 21:52:32 +0300

    Puted recommended articles from contentful

--- src/components/Blog/Recommended.tsx
+++ src/components/Blog/Recommended.tsx
@@ -1,20 +1,23 @@
-import React from "react";
+import { FC } from "react";
 
 import { SmallArticle } from "./SmallArticle";
 
-export const Recommended: React.FC = () => {
+import { SmallArticleType } from "../../@types/articles";
+
+type RecommendedProps = {
+	data: SmallArticleType[];
+};
+
+export const Recommended: FC<RecommendedProps> = ({ data }) => {
 	return (
 		<section className="recommended" aria-labelledby="recomendedSectionTitle">
 			<div className="recommended__container">
 				<h2 className="h2 recommended__title" id="recomendedSectionTitle">
 					Recommended
 				</h2>
-				<SmallArticle underline />
-				<SmallArticle underline />
-				<SmallArticle underline />
-				<SmallArticle underline />
-				<SmallArticle underline />
-				<SmallArticle underline />
+				{data.map((item) => (
+					<SmallArticle key={item.slug} underline data={item} />
+				))}
 			</div>
 		</section>
 	);
