d631eefbf29c634d0e38af3ccb762d3ac58c08e1
Артём Одийко <odiykoartem@gmail.com>
2022-10-04 17:35:19 +0300

    Added link wrapper to img

--- src/components/Blog/BigArticle.tsx
+++ src/components/Blog/BigArticle.tsx
@@ -13,16 +13,20 @@ export const BigArticle: FC<BigArticleProps> = ({ data }) => {
 	return (
 		<article className="bigArticle">
 			<div className="bigArticle__pictire">
-				<Image
-					src={data.previewImg.url}
-					alt={data.previewImg.title}
-					placeholder="blur"
-					blurDataURL="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSI+PGRlZnM+PGxpbmVhckdyYWRpZW50IGlkPSJnMSIgZ3JhZGllbnRVbml0cz0idXNlclNwYWNlT25Vc2UiIHgxPSIwJSIgeTE9IjEwMCUiIHgyPSIxMDAlIiB5Mj0iMCUiPjxzdG9wIG9mZnNldD0iMCUiIHN0b3AtY29sb3I9IiNkNWQ1ZDUiLz48c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNlMmUyZTIiLz48L2xpbmVhckdyYWRpZW50PjwvZGVmcz48cmVjdCB4PSIwIiB5PSIwIiB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJ1cmwoI2cxKSIvPjwvc3ZnPg=="
-					style={{ borderRadius: 10 }}
-					layout="fill"
-					objectFit="cover"
-					className="bigArticle__img"
-				/>
+				<Link href={`/blog/article/${data.slug}`}>
+					<a>
+						<Image
+							src={data.previewImg.url}
+							alt={data.previewImg.title}
+							placeholder="blur"
+							blurDataURL="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSI+PGRlZnM+PGxpbmVhckdyYWRpZW50IGlkPSJnMSIgZ3JhZGllbnRVbml0cz0idXNlclNwYWNlT25Vc2UiIHgxPSIwJSIgeTE9IjEwMCUiIHgyPSIxMDAlIiB5Mj0iMCUiPjxzdG9wIG9mZnNldD0iMCUiIHN0b3AtY29sb3I9IiNkNWQ1ZDUiLz48c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNlMmUyZTIiLz48L2xpbmVhckdyYWRpZW50PjwvZGVmcz48cmVjdCB4PSIwIiB5PSIwIiB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJ1cmwoI2cxKSIvPjwvc3ZnPg=="
+							style={{ borderRadius: 10 }}
+							layout="fill"
+							objectFit="cover"
+							className="bigArticle__img"
+						/>
+					</a>
+				</Link>
 			</div>
 			<div className="bigArticle__info">
 				<span className="bigArticle__tag">{data.tag.name}</span>
--- src/components/Blog/MediumArticle.tsx
+++ src/components/Blog/MediumArticle.tsx
@@ -14,16 +14,20 @@ export const MediumArticle: FC<MediumArticleProps> = ({ data }) => {
 	return (
 		<article className="mediumllArticle">
 			<div className="mediumllArticle__picture">
-				<Image
-					src={data.previewImg.url}
-					alt={data.previewImg.title}
-					placeholder="blur"
-					blurDataURL="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSI+PGRlZnM+PGxpbmVhckdyYWRpZW50IGlkPSJnMSIgZ3JhZGllbnRVbml0cz0idXNlclNwYWNlT25Vc2UiIHgxPSIwJSIgeTE9IjEwMCUiIHgyPSIxMDAlIiB5Mj0iMCUiPjxzdG9wIG9mZnNldD0iMCUiIHN0b3AtY29sb3I9IiNkNWQ1ZDUiLz48c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNlMmUyZTIiLz48L2xpbmVhckdyYWRpZW50PjwvZGVmcz48cmVjdCB4PSIwIiB5PSIwIiB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJ1cmwoI2cxKSIvPjwvc3ZnPg=="
-					style={{ borderRadius: 10 }}
-					layout="fill"
-					objectFit="cover"
-					className="mediumllArticle__img"
-				/>
+				<Link href={`/blog/article/${data.slug}`}>
+					<a>
+						<Image
+							src={data.previewImg.url}
+							alt={data.previewImg.title}
+							placeholder="blur"
+							blurDataURL="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSI+PGRlZnM+PGxpbmVhckdyYWRpZW50IGlkPSJnMSIgZ3JhZGllbnRVbml0cz0idXNlclNwYWNlT25Vc2UiIHgxPSIwJSIgeTE9IjEwMCUiIHgyPSIxMDAlIiB5Mj0iMCUiPjxzdG9wIG9mZnNldD0iMCUiIHN0b3AtY29sb3I9IiNkNWQ1ZDUiLz48c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNlMmUyZTIiLz48L2xpbmVhckdyYWRpZW50PjwvZGVmcz48cmVjdCB4PSIwIiB5PSIwIiB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJ1cmwoI2cxKSIvPjwvc3ZnPg=="
+							style={{ borderRadius: 10 }}
+							layout="fill"
+							objectFit="cover"
+							className="mediumllArticle__img"
+						/>
+					</a>
+				</Link>
 			</div>
 			<time className="mediumllArticle__time time" dateTime="09-04-2022">
 				{formatDate(data.sys.publishedAt)}
