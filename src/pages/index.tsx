82a1ed3773df43da363f2d7c62ed9e3f24b4cfe6
Артём Одийко <odiykoartem@gmail.com>
2022-10-05 10:31:18 +0300

    Added info to footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -937,6 +937,57 @@ const Home: NextPage = () => {
 							</a>
 						</div>
 					</div>
+					<div className="footer__policy">
+						<p className="footer__text">All Trademarks are the property of their respective owners.</p>
+						<p className="footer__text">
+							TOEFL® and TOEFL IBT® are registered trademarks of the{" "}
+							<a className="footer__link" href="https://www.ets.org/" target="_blank" rel="noreferrer noopener">
+								Educational Testing Service (ETS)
+							</a>
+							. This site and its owners are not affiliated, approved, or endorsed by the ETS.
+						</p>
+						<p className="footer__text">
+							DUOLINGO® and DUOLINGO ENGLISH TEST® are registered trademarks of{" "}
+							<a
+								className="footer__link"
+								href="https://englishtest.duolingo.com/"
+								target="_blank"
+								rel="noreferrer noopener"
+							>
+								Duolingo, INC
+							</a>
+							. This site and its owners are not affiliated, approved, or endorsed by Duolingo, INC.
+						</p>
+						<p className="footer__text">
+							IELTS® and IELTS Online® are registered trademarks of{" "}
+							<a className="footer__link" href="https://www.cam.ac.uk/" target="_blank" rel="noreferrer noopener">
+								University of Cambridge
+							</a>
+							, the{" "}
+							<a
+								className="footer__link"
+								href="https://www.britishcouncil.org/"
+								target="_blank"
+								rel="noreferrer noopener"
+							>
+								British Council
+							</a>
+							, and{" "}
+							<a
+								className="footer__link"
+								href="https://www.idp.com/australia/"
+								target="_blank"
+								rel="noreferrer noopener"
+							>
+								IDP Education Australia
+							</a>
+							. This site and its owners are not affiliated, approved, or endorsed by the University of Cambridge ESOL,
+							the British Council, and IDP Education Australia.
+						</p>
+						<p className="footer__text">
+							While using this site, you agree to have read and accepted our terms of use, cookie, and privacy policy.
+						</p>
+					</div>
 				</div>
 			</footer>
 		</>
